4939fecb75554e983c0435ef4b627f5217f9443d
gerry77 <operativo.cruzroja@gmail.com>
2020-11-04 16:15:17 -0600

    commit gerardo

--- src/pages/EstadoSaludCuestionario.vue
+++ src/pages/EstadoSaludCuestionario.vue
@@ -71,7 +71,7 @@
                <!-- HombroDer =1   -->
               <div v-if="idCuestionario == '1'" class="q-pa-md" id="EstadoSaludCuestionario">
                   <div class="q-gutter-xs">
-                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="1" label="¿Intenso dolor? 1" />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="1" label="¿Intenso dolor?" />
                       <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="2" label="¿Inflamación del hombro? " />
                       <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="3" label="¿Sensibilidad extrema en la zona del hombro? " />
                       <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="4" label="¿Deformidad debajo de la piel? " />
@@ -81,8 +81,7 @@
                   </div>
                 
                 <br>
-                <div class="q-px-sm">
-                 
+                <div class="q-px-sm">                 
                    <br>
                   preguntas seleccionadas {{pregunta1}}
                 </div>
@@ -97,20 +96,24 @@
             <div v-if="idCuestionario == '2'" class="q-pa-md">
                 
                 <div class="q-gutter-sm">
-                  <q-checkbox  indeterminate-value="false" v-model="pre1" label="¿Intenso dolor? 2" v-on:Change="suma(1)" />
-                  <q-checkbox indeterminate-value="maybe" v-model="pre2" label="¿Inflamación del hombro?" @checked="suma(2)" />
-                  <q-checkbox indeterminate-value="maybe" v-model="pregunta" label="¿Sensibilidad extrema en la zona del hombro?"  val="1" v-on:click="suma(2)"/>
-                  <q-checkbox indeterminate-value="maybe" v-model="pregunta" label="¿Deformidad debajo de la piel?"  val="2"/>
-                  <q-checkbox indeterminate-value="maybe" v-model="pregunta" label="¿Coloración violácea o decoloración de la piel?" />
-                  <q-checkbox indeterminate-value="maybe" v-model="pregunta" label="¿Crujidos?" />
-                  <q-checkbox indeterminate-value="maybe" v-model="pregunta" label="¿Incapacidad de mover el brazo sin experimentar dolor?" />
+              <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="1" label="¿Intenso dolor?" />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="2" label="¿Inflamación del hombro? " />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="3" label="¿Sensibilidad extrema en la zona del hombro? " />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="4" label="¿Deformidad debajo de la piel? " />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="5" label="¿Coloración violácea o decoloración de la piel?" />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="6" label="¿Crujidos?" />
+                      <q-checkbox indeterminate-value="maybe" v-model="pregunta1" val="7" label="¿Incapacidad de mover el brazo sin experimentar dolor?" />
                 </div>
                 <br>
-                <div class="q-px-sm">
-                  Total: <strong>{{pregunta}}</strong>
+               <div class="q-px-sm">                 
+                   <br>
+                  preguntas seleccionadas {{pregunta1}}
                 </div>
                  <br>
-                <q-btn  color="primary" label="Enviar" @click="Recomendacion" />               
+                 <div >
+                      <p> Total: {{total}}</p>
+                 </div>
+                <q-btn color="primary" label="Enviar" @click="Recomendacion(total,idCuestionario)" @mouseenter="enviar"/>                  
               </div>
 
               
@@ -130,27 +133,9 @@ export default {
     data() {
       return {
         idCuestionario: this.$route.params.id,
-        pregunta:['algo'],
-        pregunta1: [],
-        pre1:null,
-        pre2:null,
-        pre3:null,
-        pre4:null,
-        pre5:null,
-        pre6:null,
-        pre7:null,        
-        total: 0,
-        check: 1,
-        valor:0,
-        preguntasHombro:[{value:null,text:"¿Intenso dolor?"},
-                   {value:null,text:"¿Inflamación del hombro?"},
-                   {value:null,text:"¿Sensibilidad extrema en la zona del hombro?"},
-                   {value:null,text:"¿Deformidad debajo de la piel?"},
-                   {value:null,text:"¿Coloración violácea o decoloración de la piel?"},
-                   {value:null,text:"¿Crujidos?"},
-                   {value:null,text:"¿Incapacidad de mover el brazo sin experimentar dolor?"}
-                  ]
-      }
+        pregunta1: [],           
+        total: 0     
+        }
     },
     methods:{
       enviar(){
@@ -162,8 +147,9 @@ export default {
         atras(){
             this.$router.go(-1)
         },
-         Recomendacion(){
-            this.$router.push('/EstadoSaludRecomendacion')
+         Recomendacion(score,areaAfectada){
+           console.log(score+'  '+areaAfectada)
+            this.$router.push('/EstadoSaludRecomendacion'+score+'/'+areaAfectada)
         },
         reset () {
       this.pre1 = null
@@ -174,23 +160,8 @@ export default {
       this.pre6 = null
       this.pre7 = null      
       } ,
-       suma(valor) {
-       // se debe parsear porque Vue transforma el valor vacío a string
-       this.total=20
-       console.log("entre a suma")
-       if (this.pre1===true) {suma=Number(this.pre1.id) }
-       if (this.pre2===true) {suma=Number(this.pre1.id) }
-       if (this.pre3===true) {suma=Number(this.pre1.id) }
-       return this.total
-       }   
-              
-    },
-     computed: {
-      algo(){
-        console.log("entre algo")
-        this.total=this.total+ this.pregunta.values
-        return this.total
-      },    
+       
+       
    },
   components: {
     Footer
--- src/router/routes.js
+++ src/router/routes.js
@@ -47,7 +47,7 @@ const routes = [
   {path: '/Repetir', component: () => import('pages/Repetir.vue')},
   {path: '/personalizado', component: () => import('pages/personalizado.vue')},
   {path: '/EstadoSaludCuestionario/:id', component: () => import('pages/EstadoSaludCuestionario.vue')},
-  {path: '/EstadoSaludRecomendacion', component: () => import('pages/EstadoSaludRecomendacion.vue')},  
+  {path: '/EstadoSaludRecomendacion/:total:areaAfectada', component: () => import('pages/EstadoSaludRecomendacion.vue')},  
   {path: '/seguimientoGraficas', component: () => import('pages/seguimientoSaludGraficas.vue')},
   
 ]
